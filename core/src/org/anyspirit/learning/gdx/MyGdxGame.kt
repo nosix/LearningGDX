902aec455258139da3d7223f33a2a410e229185c
nosix <i.nosix@gmail.com>
2017-01-04 18:22:55 +0900

    added Kotlin standard library

--- core/src/org/anyspirit/learning/gdx/MyGdxGame.kt
+++ core/src/org/anyspirit/learning/gdx/MyGdxGame.kt
@@ -1,30 +1,82 @@
 package org.anyspirit.learning.gdx
 
+import com.badlogic.gdx.Application
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.utils.Scaling
+import com.badlogic.gdx.utils.viewport.ScreenViewport
 
 class MyGdxGame : ApplicationAdapter() {
-    private lateinit var batch: SpriteBatch
-    private lateinit var img: Texture
+
+    companion object {
+        private val TAG = MyGdxGame::class.java.name
+    }
+
+    private val stage by lazy {
+        Stage(ScreenViewport())
+        /*
+        Stage(ScreenViewport().apply {
+            unitsPerPixel = 2f
+        })
+        */
+        //Stage(ScalingViewport(Scaling.none, 640f, 480f))
+        //Stage(ScalingViewport(Scaling.fit, 640f, 480f))
+        //Stage(FitViewport(640f, 480f))
+        //Stage(ScalingViewport(Scaling.fill, 640f, 480f))
+        //Stage(FillViewport(640f, 480f))
+        //Stage(ScalingViewport(Scaling.stretch, 640f, 480f))
+        //Stage(StretchViewport(640f, 480f))
+        //Stage(ExtendViewport(640f, 480f))
+        //Stage(ExtendViewport(640f, 480f, 800f, 480f))
+        //Stage(ExtendViewport(640f, 480f, 800f, 640f))
+    }
+
+    private val image by lazy {
+        Image(Texture("badlogic.jpg")).apply {
+            //setScaling(Scaling.none)
+            //setScaling(Scaling.fit)
+            //setScaling(Scaling.fill)
+            //setScaling(Scaling.stretch)
+            //setAlign(Align.center)
+        }
+    }
 
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        Gdx.app.logLevel = Application.LOG_DEBUG
+        Gdx.app.debug(TAG, "create")
+        Gdx.graphics.isContinuousRendering = false
+        Gdx.input.inputProcessor = stage
+        stage.setDebugAll(true)
+        stage.addActor(image)
+    }
+
+    override fun pause() {
+        Gdx.app.debug(TAG, "pause")
+    }
+
+    override fun resume() {
+        Gdx.app.debug(TAG, "resume")
+    }
+
+    override fun resize(width: Int, height: Int) {
+        Gdx.app.debug(TAG, "resize $width $height")
+        stage.viewport.update(width, height, true)
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.app.debug(TAG, "render")
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
+        stage.act(Gdx.graphics.deltaTime)
+        stage.draw()
     }
 
     override fun dispose() {
-        batch.dispose()
-        img.dispose()
+        Gdx.app.debug(TAG, "dispose")
+        stage.dispose()
     }
 }
--- html/src/org/anyspirit/learning/gdx/client/DragomeConfiguration.kt
+++ html/src/org/anyspirit/learning/gdx/client/DragomeConfiguration.kt
@@ -5,8 +5,10 @@ import com.dragome.commons.DragomeConfiguratorImplementor
 @DragomeConfiguratorImplementor(priority = 20)
 class DragomeConfiguration : com.badlogic.gdx.backends.dragome.DragomeConfiguration() {
 
-    override fun projectClassPathFilter(projectPath: String): Boolean =
-            ".*/core-[0-9.]+.jar".toRegex().matches(projectPath)
+    override fun projectClassPathFilter(projectPath: String): Boolean = false or
+            ".*/core-[0-9.]+.jar".toRegex().matches(projectPath) or
+            ".*/kotlin-stdlib-[0-9.-]+.jar".toRegex().matches(projectPath) or
+            ".*/kotlin-runtime-[0-9.-]+.jar".toRegex().matches(projectPath)
 
     override fun isRemoveUnusedCode(): Boolean = true
 }
